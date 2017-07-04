28d70937afd110914e86cddd2a718ae8a092c65c
György Mátyás <gy.matyas97@gmail.com>
2017-07-04 10:06:05 +0200

    Deleted conflict text

--- gittie.py
+++ gittie.py
@@ -1,11 +1,8 @@
 def introduce():
     print("Hello,I'm Gittie!")
 
-<<<<<<< HEAD
 def add(a, b):
     return a + b
-=======
 
 def joke():
     print("What do you call a fish with no eyes? A fsh.")
->>>>>>> 96d7e51a70d2470523b2e89bf5c1ea410eb6a96c
